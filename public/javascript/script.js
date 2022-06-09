ae820f9863a5af7e0cdb1f89b6fac53f86e7c469
Alex Vazquez <avazque008@outlook.com>
2022-06-09 15:51:25 -0400

    arrays and checkboxes created

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,7 +1,25 @@
+// Criteria Checkboxes
+const lowercase = document.getElementById('lowercase');
+const uppercase = document.getElementById('uppercase');
+const numbers = document.getElementById('numbers');
+const specialCharacters = document.getElementById('special-characters');
+
+// Criteria Array
+
+const uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowercaseArr = uppercaseArr.map(letter => letter.toLowerCase());
+const numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialCharArr = ['"', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '~'];
+
+
 // Generate Password
 
+let generatePasswordText = '';
 
+function generatePassword() {
 
+  return uppercaseArr.concat(lowercaseArr).concat(numbersArr).concat(specialCharArr);
+};
 
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
