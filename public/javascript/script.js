050cb6225d8bea97df8a1225d89d8692d227500b
Alex Vazquez <avazque008@outlook.com>
2022-06-09 21:00:27 -0400

    new function created for criteria

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -3,6 +3,7 @@ const lowercase = document.getElementById('lowercase');
 const uppercase = document.getElementById('uppercase');
 const numbers = document.getElementById('numbers');
 const specialCharacters = document.getElementById('special-characters');
+const length = document.getElementById('length');
 
 // Criteria Array
 
@@ -12,20 +13,46 @@ const numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialCharArr = ['"', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '~'];
 
 
+// Generate Password Array
+
+function generatePasswordArr() {
+
+  let passwordArr = [];
+
+  if (!lowercase.checked && !uppercase.checked && !numbers.checked && !specialCharacters.checked) {
+    alert('Choose what to include in your password!');
+    return '';
+  }
+
+  if (lowercase.checked) {
+    lowercaseArr.forEach(letter => passwordArr.push(letter));
+  }
+
+  if (uppercase.checked) {
+    uppercaseArr.forEach(letter => passwordArr.push(letter));
+  }
+
+  if (numbers.checked) {
+    numbersArr.forEach(letter => passwordArr.push(letter));
+  }
+
+  if (specialCharacters.checked) {
+    specialCharArr.forEach(letter => passwordArr.push(letter));
+
+  }
+  
+  return passwordArr;
+};
+
 // Generate Password
 
-let generatePasswordText = '';
-
-function generatePassword() {
-
-  return uppercaseArr.concat(lowercaseArr).concat(numbersArr).concat(specialCharArr);
-};
 
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
 
